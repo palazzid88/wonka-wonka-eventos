45ac66ff1e80cfc79cd08b57022c3db403af31cb
palazzid88 <palazzid88@gmail.com>
2025-09-01 14:09:03 -0300

    se trabaja en SEO

--- script.js
+++
@@ -1,6 +0,0 @@
-  const burger = document.querySelector('.burger');
-  const navMenu = document.querySelector('.navbar ul');
-
-  burger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-  });
